f39065c4a1e13852fdb1e71efc0e765c53390808
Jocelyn Thai <jocelyn.thai@upandup.co>
2025-07-16 16:43:14 -0400

    use prod database

--- src/tabs/data.py
+++ src/tabs/data.py
@@ -5,7 +5,7 @@ import pandas as pd
 @st.cache_data
 def get_bad_debt_inputs_data(_credentials):
     bad_debt_inputs = pd.read_gbq(f"SELECT * \
-                        FROM `homevest-data.dbt_kyanscienceman_tin.bad_debt_inputs` \
+                        FROM `homevest-data.dbt_prod_tin.bad_debt_inputs` \
                         WHERE month >= DATE_SUB(LAST_DAY(CURRENT_DATE(), MONTH), INTERVAL 11 MONTH) \
                         AND month <= LAST_DAY(CURRENT_DATE(), MONTH)", credentials=_credentials)
     # Convert month to first of the month for charting purposes
@@ -17,6 +17,6 @@ def get_bad_debt_inputs_data(_credentials):
 @st.cache_data
 def get_collections_curve_data(_credentials):
     collections_curve_data = pd.read_gbq(f"SELECT * \
-                        FROM `homevest-data.dbt_kyanscienceman_tin.rent_collections_curve`", credentials=_credentials)
+                        FROM `homevest-data.dbt_prod_tin.rent_collections_curve`", credentials=_credentials)
     return collections_curve_data
 
