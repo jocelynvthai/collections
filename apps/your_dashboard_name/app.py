f17382ccc83688e6f312e7cfbcca98428d6e44c7
Jocelyn Thai <jocelyn.thai@upandup.co>
2025-07-24 18:00:39 -0400

    leasing dashboard invh

--- apps/your_dashboard_name/app.py
+++ apps/your_dashboard_name/app.py
@@ -3,6 +3,14 @@ from google.oauth2 import service_account
 
 from data import get_service_account_info, get_data
 
+# Configure page layout
+st.set_page_config(
+    page_title="Dashboard Name",
+    page_icon="",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
 # Data Retrieval
 credentials = service_account.Credentials.from_service_account_info(get_service_account_info(local=True))
 invitation_homes_data = get_data(credentials)
